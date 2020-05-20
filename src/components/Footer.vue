8a19869bea1d196485090dd2f01737382a6da9f2
Mehtaab Gill <37054853+mehtaabGill@users.noreply.github.com>
2020-05-19 23:21:15 -0600

    added contact method

---
+++ src/components/Footer.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="w-screen md:w-3/4 py-4 mx-auto">
+      <a href="mailto:mehtaabgill@hotmail.com" class="text-purple-400 font-bold">Contact Site Owner</a>
+  </div>
+</template>
+
+<script>
+export default {
+    name:'Footer'
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,6 +4,7 @@
     <Errors class="mx-auto"/>
     <h1 class="mx-auto text-gray-800" v-if="isNarrow()">*please view the site on a laptop or pc for the best view*</h1>
     <DataTable class="mx-auto"/>
+    <Footer/>
   </div>
 </template>
 
@@ -12,13 +13,15 @@
 import DataTable from '@/components/DataTable.vue'
 import Header from '@/components/Header.vue'
 import Errors from '@/components/Errors.vue'
+import Footer from '@/components/Footer.vue'
 
 export default {
   name: 'Home',
   components: {
     DataTable,
     Header,
-    Errors
+    Errors,
+    Footer
   },
   mounted() {
     this.$store.dispatch('loadData');
